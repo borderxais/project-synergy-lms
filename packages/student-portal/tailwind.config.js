90e1caa18701827f24386a5a6a34b783f333a4f0
ysun9487 <ysun9487@usc.edu>
2025-07-14 21:21:16 -0700

    config

--- packages/student-portal/tailwind.config.js
+++ packages/student-portal/tailwind.config.js
@@ -1,12 +1,11 @@
-// packages/student-portal/tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  };
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
